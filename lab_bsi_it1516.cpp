23bf741f6050373208a4ebe74e2187143110bdd8
Stryapunin-Irmuhambetova-IT-15-16 <strapuninirmuhambetova@gmail.com>
2024-12-11 14:31:52 +0500

    Update lab_bsi_it1516.cpp

--- lab_bsi_it1516.cpp
+++ lab_bsi_it1516.cpp
@@ -1,4 +1,4 @@
-﻿#include "Header.h"
+#include "Header.h"
 #include <iostream>
 #include <cmath>
 
@@ -6,13 +6,34 @@ using namespace std;
 
 
 int main() {
-    int A, B, i;
-    cout << "Введите число ";
-    cin >> A; cout << endl;
-    B = randomnumber();
-    switch (proverka(A, B)) {
-    case 1: cout << "Верно!"; break;
-    case 2: cout << "Не то число, нужное - больше "; break;
-    case 3: cout << "Не то число, нужное - меньше "; break;
+    setlocale(LC_ALL, "RUS");
+
+    int A, B;
+    int zero = 0;
+    int one = 0;
+    int two = 0;
+    bool pop = false;
+    cout << "Введите четырехзначное число ";
+    cin >> A ;
+    cout << endl;
+
+    while (pop == false){
+        B = randomnumber();
+        int N = proverka (A, B);
+        while (N > 0) {
+            int digit = N % 10;
+            if (digit == 0) zero++;
+            else if (digit == 1) one++;
+            else if (digit == 2) two++;
+            N /= 10;
+        }
+        if (zero == 4) cout << "Быков и коров нет, попробуйте ещё раз :(";
+        else if (two == 4) {
+            cout << "Правильно! Вы выйграли! Ура!";
+            pop = true;
+        }
+        else {
+            cout << "Вы на пути к правильному ответу! Коров - " <<  one << " , a быков - " << two;
+        }
     }
 }
