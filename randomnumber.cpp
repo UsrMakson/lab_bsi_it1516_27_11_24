8bf88c7c6b245468079b78c9e7d12f6c4082fa32
Бочкарев ИТ-16 <81972957+AdReLaN@users.noreply.github.com>
2024-12-11 14:11:04 +0500

    Модульный файл с функцией генерации рандомного числа

---
+++ randomnumber.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+#include <ctime>
+#include <random>
+
+using namespace std;
+
+int randomnumber() {
+	srand(time(0));
+	int f[]{ 1,2,3,4,5,6,7,8,9 }; int d = 1; int ind = rand() % 9;
+	int number = 0;
+	for (int i = 0; i < 4; i++) {
+		while (f[ind] == 0) {
+			ind = rand() % 9;
+		}
+		number = number + d * f[ind];
+		d *= 10;
+		f[ind] = 0;
+	}
+	return number;
+}
